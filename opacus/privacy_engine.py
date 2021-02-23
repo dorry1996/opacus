92111c6cd019e300e7844830f2630e4ed81aa7d1
Wenzhe Zhao <46192647+dorry1996@users.noreply.github.com>
2021-02-23 18:55:09 +0800

    Update privacy_engine.py

--- opacus/privacy_engine.py
+++ opacus/privacy_engine.py
@@ -309,10 +309,10 @@ class PrivacyEngine:
 #                 f"but received a batch of size {batch_size}"
 #             )
         # 自己添加一个可以查看 batch_size的代码 可以发现一个周期内的迭代次数就是样本数/batch_size
-        print(self.steps)
+#         print(self.steps)
 #         if batch_size > self.batch_size:
-        print('batch_size:',batch_size)
-        print('self.batch_size:',self.batch_size)
+#         print('batch_size:',batch_size)
+#         print('self.batch_size:',self.batch_size)
 
         if batch_size < self.batch_size:
             warnings.warn(
